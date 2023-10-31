88694c18d3f48bf97c3aefd298298a45048c5753
Peyton Kellogg <peytonkellogg@gmail.com>
2023-10-30 19:48:51 -0700

    stubs initial db_seeds_helper and test for scraping functionality

---
+++ app/helpers/db_seeds_helper.rb
@@ -0,0 +1,9 @@
+module DbSeedsHelper
+  def base_google_url
+    "https://play.google.com/store/apps"
+  end
+
+  def base_apple_url
+    "https://apps.apple.com/us/app"
+  end
+end
---
+++ spec/helpers/db_seeds_helper.rb
@@ -0,0 +1,6 @@
+# load the configuration for RSpec
+require 'rails_helper'
+
+RSpec.describe DbSeedsHelper do
+  # TODO test stuff; see: https://remimercier.com/rspec-101-basic-set-up/
+end
