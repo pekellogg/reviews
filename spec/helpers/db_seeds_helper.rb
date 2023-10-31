3ebf4f84ba1d7af3c9720019ab14feb2d003b8a7
Peyton Kellogg <peytonkellogg@gmail.com>
2023-10-30 19:49:53 -0700

    ammends TODO note

--- spec/helpers/db_seeds_helper.rb
+++ spec/helpers/db_seeds_helper.rb
@@ -2,5 +2,5 @@
 require 'rails_helper'
 
 RSpec.describe DbSeedsHelper do
-  # TODO test stuff; see: https://remimercier.com/rspec-101-basic-set-up/
+  # TODO test stuff; see: https://remimercier.com/rspec-101-basic-set-up/, https://www.rubyguides.com/2020/01/rails-helpers/
 end
